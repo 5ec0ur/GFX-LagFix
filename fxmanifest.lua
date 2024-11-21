2416bad9d8906eda3c59b83243e83ba9f1e9b45d
5ec0ur <169008855+5ec0ur@users.noreply.github.com>
2024-11-21 11:16:33 +0000

    Create fxmanifest.lua
    Added All Info For Fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'Cerulean'
+game 'gta5'
+
+Author 'GFX'
+Description 'Reduce Server Lag (Changing Values for the densitys of Traffic , Peds And Animals)'
+
+Version '0.0.1'
+
+client_script 'client.lua'
